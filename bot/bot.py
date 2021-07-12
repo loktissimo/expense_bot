a624050c9a49941df5565a65fc6675e6cbc1c26a
loktissimo <loktissimo@example.com>
2021-07-12 23:13:04 +0300

    beta version

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,6 @@
+from mysql.connector.errors import Error
 import telebot
+from telebot.types import ReplyKeyboardRemove
 from db import query_db
 from dotenv import load_dotenv
 import os
@@ -7,57 +9,58 @@ load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# Texts
+message_start_text = "Привет\\!\nДля начала представься, напиши:``` /name Иванов Иван``` "
+message_ok_text = "Записывай свои траты. Уже можно."
+message_done_text = "Готово! Можешь записывать траты."
+message_insert_text = "Записал!"
+message_phone_text = "Передай номер, пожалуйста."
 
 # SQL queries
 add_user = "INSERT INTO users (telegram_id, name) VALUES (%s, %s)"
+add_phone = "UPDATE users SET tel = %s WHERE telegram_id = %s"
 check_user_exist = "SELECT * FROM users WHERE telegram_id = %s"
-add_expense = "INSERT INTO expense (telegram_id, text) VALUES (%s, %s)"
+add_expense = "INSERT INTO expense (telegram_id, text, write_date) VALUES (%s, %s, now())"
 
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    # bot.reply_to(message, "Hello, how are you doing?")
     id = message.chat.id
-    text = message.text
-    if query_db(check_user_exist, (id,)):
-        bot.send_message(id, "Записывай свои траты. Уже можно.")
+    name = query_db(check_user_exist, (id,))
+    if name:
+        bot.send_message(id, message_ok_text)
     else:
-        text = "Привет\\!\nДля начала представься, напиши:``` /name Иванов Иван ``` "
-        bot.send_message(id, text, parse_mode="MarkdownV2")
+        bot.send_message(id, message_start_text, parse_mode="MarkdownV2")
 
 
 @bot.message_handler(commands=["name"])
 def get_name(message):
     # bot.reply_to(message, "Hello, how are you doing?")
     id = message.chat.id
-    text = message.text
+    text = message.text.lstrip("/name ")
     name = query_db(check_user_exist, (id,))
     if name:
-        bot.send_message(id, "Записывай свои траты. Уже можно.")
+        bot.send_message(id, message_ok_text)
+    elif not text:
+        bot.send_message(id, message_start_text, parse_mode="MarkdownV2")
     else:
-        query_db(add_user, (id, text.lstrip("/name ")))
+        query_db(add_user, (id, text))
+
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        reg_button = telebot.types.KeyboardButton(
+            text="Передать номер телефона", request_contact=True)
+        markup.add(reg_button)
         bot.send_message(
-            id, "Отлично\\! Можно записывать траты\\.", parse_mode="MarkdownV2",
-            request_contact=True
-        )
-
-
-@bot.message_handler(commands=["reg"])
-def register(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(
-        one_time_keyboard=True, resize_keyboard=True, row_width=1)
-    reg_button = telebot.types.KeyboardButton(
-        text="Зарегистрироваться", request_contact=True
-    )
-    keyboard.add(reg_button)
-    bot.send_message(
-        message.chat.id, "You should share your phone number", reply_markup=keyboard
-    )
+            message.chat.id, message_phone_text, reply_markup=markup)
 
 
 @bot.message_handler(content_types=["contact"])
 def contact_handler(message):
-    print(message.contact)
+    id = message.chat.id
+    phone = message.contact.phone_number
+    query_db(add_phone, (phone, id))
+    bot.send_message(
+        message.chat.id, message_done_text, reply_markup=ReplyKeyboardRemove())
 
 
 @bot.message_handler(func=lambda message: True)
@@ -66,11 +69,13 @@ def get_all(message):
     text = message.text
     name = query_db(check_user_exist, (id,))
     if name:
-        query_db(add_expense, (id, text))
-        bot.reply_to(message, "Записал!")
+        try:
+            query_db(add_expense, (id, text))
+            bot.reply_to(message, message_insert_text)
+        except Error as e:
+            bot.reply_to(message, e)
     else:
-        text = "Привет\\!\nДля начала представься, напиши:``` /name Иванов Иван ``` "
-        bot.send_message(id, text, parse_mode="MarkdownV2")
+        bot.send_message(id, message_start_text, parse_mode="MarkdownV2")
 
 
 bot.polling()
